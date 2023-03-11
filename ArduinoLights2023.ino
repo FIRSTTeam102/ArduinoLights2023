5b8810be6e545eb56bacb852f67648eeecf24cf7
Calum Dingwall <caburum@users.noreply.github.com>
2023-03-11 13:05:13 -0500

    finalization & comp changes

--- ArduinoLights2023.ino
+++ ArduinoLights2023.ino
@@ -3,11 +3,11 @@
 
 #define PIN_DATA 4 // Arduino pin that connects to strip data
 
-#define NUM_PIXELS 120 // The number of LEDs (pixels) on strip
+#define NUM_PIXELS 60 // The number of LEDs (pixels) on strip
 #define DELAY_INTERVAL 20
 
 #define SATURATION 255
-#define VALUE 255
+#define VALUE 130
 
 #define YELLOW_HUE 10082
 #define BLUE_HUE 43690
@@ -16,217 +16,197 @@
 #define PURPLE_HUE 51651
 #define ORANGE_HUE 4554
 #define GREEN_HUE 21845
+#define CORAL_HUE 62804
+#define CYAN_HUE 32404
 
-#define NUM_ACTIVE_GROUPS 5
-int groupHues[] = {0, BLUE_HUE, PINK_HUE, ORANGE_HUE, GREEN_HUE, RED_HUE};
+#define NUM_ACTIVE_GROUPS 6
+// retroreflective, apriltag, coral, ele/arm, grabber, HP
+int groupHues[] = {0, GREEN_HUE, ORANGE_HUE, CORAL_HUE, BLUE_HUE, CYAN_HUE, 0};
 
 Adafruit_NeoPixel strip(NUM_PIXELS, PIN_DATA, NEO_GRB + NEO_KHZ800);
 
-
-//co-function to assist comet
 int wrapPixel(int pixel) {
-  return pixel < 0
-         ? NUM_PIXELS + pixel
-         : pixel;
+	return pixel < 0
+		? NUM_PIXELS + pixel
+		: pixel;
 }
 
-//default comet
+// not breathing comet
 void comet(int hue, int saturation, int value) {
 	int tailFormula;
-	int tail = 9;
+	int tail = 13;
 	for (int pixel = 0; pixel < NUM_PIXELS; pixel++) { // locate pixel
 		for (int i = 0; i < tail; i++) {
 			tailFormula = value / tail;
-			strip.setPixelColor(pixel - i, strip.gamma32(strip.ColorHSV(hue, saturation, value))); // colors front of the comet
+			if (i < 6) // first 6 should be max value
+				strip.setPixelColor(pixel - i, strip.gamma32(strip.ColorHSV(hue, saturation, value))); // colors front of the comet
+			else // next 7 should scale down to 0 value based on position in tail (further back = less intense)
+				strip.setPixelColor(pixel - i, strip.gamma32(strip.ColorHSV(hue, saturation, value - ((i - 6) * (value / 7)))));
 			strip.setPixelColor(wrapPixel(pixel - tail), strip.Color(0, 0, 0)); // clears end of tail
 			strip.show();
 		}
 	}
 }
 
-// breathing comet
-void breatingComet(int hue, int saturation, int value) {
-	bool increase;
-  int tailFormula;
-  int Value = 0;
-  int tail = 9;
-  int cyclePixel;
-  bool breatheStatus;  //true = breathe in, false = breathe out
-  for (int pixel = 0; pixel < NUM_PIXELS; pixel++) { // locate pixel
-  	cyclePixel = pixel % 30;
-    for (int i = 0; i < tail; i++) {
-    	if(cyclePixel>15){
-    		breatheStatus = false;
-    	}
-    	else{
-    		breatheStatus = true;
-    	}
-    	if(breatheStatus == false){
-    		strip.setPixelColor(pixel - i, strip.gamma32(strip.ColorHSV(hue, saturation, 255-(cyclePixel * 17)))); // colors front of the comet
-    	}
-    	else{
-    		strip.setPixelColor(pixel - i, strip.gamma32(strip.ColorHSV(hue, saturation, cyclePixel * 17))); // colors front of the comet
-    	}
-      strip.setPixelColor(wrapPixel(pixel - tail), strip.Color(0, 0, 0)); // clears end of tail
-      strip.show();	
-    }
-  }
+// breathing comet (comet fades in and out)
+#define BC_TAIL 11
+void breathingComet(int hue, int saturation, int value) {
+	int cyclePixel;	// pixel in the current section of the strip
+	for (int pixel = 0; pixel < NUM_PIXELS; pixel++) { // locate pixel
+		cyclePixel = pixel % 30;
+		for (int i = 0; i < BC_TAIL; i++) {
+			// true = breathe in, false = breathe out
+			strip.setPixelColor(pixel - i, strip.gamma32(strip.ColorHSV(hue, saturation, cyclePixel > 15
+				? (255 -(cyclePixel * 17))
+				: (cyclePixel * 17)))); // colors front of the comet
+			strip.setPixelColor(wrapPixel(pixel - BC_TAIL), strip.Color(0, 0, 0)); // clears end of tail
+			strip.show();
+		}
+	}
 }
 
-//fade strip in and out
-void breathe(int hue,int saturation, int value){
-	int Saturation = 0;
-	bool increase;
-	int i;
-	for(i = 0; i < 255; i++){
-		strip.fill(strip.gamma32(strip.ColorHSV(hue,saturation,i)));
+// fade strip in and out
+void breathe(int hue,int saturation, int value) {
+	for (int i = 0; i < 255; i++) {
+		strip.fill(strip.gamma32(strip.ColorHSV(hue, saturation, i)));
 		strip.show();
 	}
-	for(i = 255; i>0; i--){
-		strip.fill(strip.gamma32(strip.ColorHSV(hue,saturation,i)));
+	for (int i = 255; i > 0; i--) {
+		strip.fill(strip.gamma32(strip.ColorHSV(hue, saturation, i)));
 		strip.show();
 	}
 }
 
-
-
-
-//Call for cone or cube
-void fillStrip_RGB(int RED, int GREEN, int BLUE){
-	for(int i=0; i<=NUM_PIXELS;i++){
-		strip.setPixelColor(i, RED, GREEN, BLUE);			
+void fillStrip_RGB(int r, int g, int b){
+	for (int i = 0; i <= NUM_PIXELS; i++) {
+		strip.setPixelColor(i, r, g, b);
 		strip.show();
 	}
 }
 
-
-//basic blink
+// basic blink
 void blink(int hue, int saturation, int value) {
-  static bool toggle = false; // false=odd, true=even
-  uint32_t color = strip.ColorHSV(hue, saturation, value);
-  uint32_t off = strip.ColorHSV(0, 0, 0);
-  uint32_t evenColor;
-  uint32_t oddColor;
+	static bool toggle = false; // false=odd, true=even
+	uint32_t color = strip.ColorHSV(hue, saturation, value);
+	uint32_t off = strip.ColorHSV(0, 0, 0);
+	uint32_t evenColor;
+	uint32_t oddColor;
 
-  if (toggle == false) {
-    oddColor = color;
-    evenColor = off;
-  } else {
-    evenColor = color;
-    oddColor = off;
-  }
-  toggle = !toggle;
+	if (toggle == false) {
+		oddColor = color;
+		evenColor = off;
+	} else {
+		evenColor = color;
+		oddColor = off;
+	}
+	toggle = !toggle;
 
-  for (int i = 0; i < NUM_PIXELS; i++) {
-    if (i % 2 == 0) { // even
-      strip.setPixelColor(i, evenColor);
-    } else {
-      strip.setPixelColor(i, oddColor);
-    }
-  }
-  strip.show();
-  // delay(1000); // fixme: use counter
+	for (int i = 0; i < NUM_PIXELS; i++) {
+		if (i % 2 == 0) { // even
+			strip.setPixelColor(i, evenColor);
+		} else {
+			strip.setPixelColor(i, oddColor);
+		}
+	}
+	strip.show();
+	// delay(1000); // fixme: use counter
 }
-
-
 
 void rave(int saturation, int value) {
-  uint32_t hue = random(65536);
-  uint32_t color = strip.ColorHSV(hue, saturation, value);
-  for(int j = 0; j<NUM_PIXELS*4; j++){
-  	for (int i = 0; i < NUM_PIXELS; i++) {
-    	strip.setPixelColor(i, color);
-  	}
-  }
-  strip.show();
-  // delay(250); // fixme: use counter
+	uint32_t hue = random(65536);
+	uint32_t color = strip.ColorHSV(hue, saturation, value);
+	for (int j = 0; j < NUM_PIXELS * 20; j++) {
+		for (int i = 0; i < NUM_PIXELS; i++) {
+			strip.setPixelColor(i, color);
+		}
+	}
+	strip.show();
+	// delay(250); // fixme: use counter
 }
 
 void setDirectControlPixel(byte group, int pixel, bool on) {
-  strip.setPixelColor(pixel, strip.ColorHSV(groupHues[group], 255, on ? 255 : 1));
+	uint32_t color = strip.gamma32(strip.ColorHSV(groupHues[group], 255, on ? 255 : 0));
+	strip.setPixelColor(pixel, color);
+	strip.setPixelColor(pixel + 1, color);
 }
 
 void setGroup(int startPixel, int hue) {
-  strip.setPixelColor(startPixel, strip.ColorHSV(hue, 255, 255));
-  strip.setPixelColor(startPixel + 1, strip.ColorHSV(hue, 255, 255));
-  strip.setPixelColor(startPixel + 2, strip.ColorHSV(hue, 255, 255));
+	for (int i = 0; i < 6; i++) {
+		strip.setPixelColor(startPixel + i, strip.ColorHSV(hue, 255, 255));
+	}
 }
 
 void setup() {
-  pinMode(SS, INPUT); // chip select
-  pinMode(SCK, INPUT); // clock
-  pinMode(MOSI, INPUT); // master out *slave in*
-  pinMode(MISO, OUTPUT); // master in *slave out*
+	pinMode(SS, INPUT); // chip select
+	pinMode(SCK, INPUT); // clock
+	pinMode(MOSI, INPUT); // master out *slave in*
+	pinMode(MISO, OUTPUT); // master in *slave out*
 
-  // set SPI control register
-  SPI.attachInterrupt();
-  SPCR |= _BV(SPE); // enable SPI
-  SPI.setBitOrder(MSBFIRST);
-  SPCR &= ~_BV(MSTR); // slave
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV4);
+	// set SPI control register
+	SPI.attachInterrupt();
+	SPCR |= _BV(SPE); // enable SPI
+	SPI.setBitOrder(MSBFIRST);
+	SPCR &= ~_BV(MSTR); // slave
+	SPI.setDataMode(SPI_MODE0);
+	SPI.setClockDivider(SPI_CLOCK_DIV4);
 
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  strip.begin();
-  strip.clear();
+	strip.begin();
+	strip.clear();
 }
-volatile byte statuses[16] = {3, 1, 1, 1, 1, 1};
+
+volatile byte statuses[16] = {0};
 // SPI interrupt routine
 ISR(SPI_STC_vect) {
-  // SPDR is the incoming byte
-  statuses[SPDR & 0b1111] = SPDR >> 4;
+	// SPDR is the incoming byte
+	statuses[SPDR & 0b1111] = SPDR >> 4;
 }
 
-
 void loop() {
-  switch (statuses[0]) { // control mode
-    // regular
-    case 2:
-      for (byte group = 1; group <= NUM_ACTIVE_GROUPS; group++) {
-        byte state = statuses[group];
-        for (int pix = (group - 1) * 3; pix < NUM_PIXELS; pix += (3 * NUM_ACTIVE_GROUPS)) {
-          if (state & 0b1000 != 0) {
-            // direct control
-            setDirectControlPixel(group, pix, (state & 0b100) != 0);
-            setDirectControlPixel(group, pix + 1, (state & 0b010) != 0);
-            setDirectControlPixel(group, pix + 2, (state & 0b001) != 0);
-          } else if (state == 0b1000) {
-            // cone
-            setGroup(pix, YELLOW_HUE);
-          } else if (state == 0b1001) {
-            // cube
-            setGroup(pix, PURPLE_HUE);
-          }
-        }
-      }
-      strip.show();
-      break;
-
-    // disabled
-    case 3:
-      comet(RED_HUE, SATURATION, VALUE);
-      break;
-    case 4:
-      comet(BLUE_HUE, SATURATION, VALUE);
-      break;
-		case 5:  //cube
-			fillStrip_RGB(159,23,169);
+	switch (statuses[0]) { // control mode
+		// regular
+		case 2:
+			for (byte group = 1; group <= NUM_ACTIVE_GROUPS; group++) {
+				Serial.print("G");
+				Serial.print(group);
+				Serial.print(" ");
+				Serial.println((group - 1) * 6);
+				byte state = statuses[group];
+				for (int pix = (group - 1) * 6; pix < NUM_PIXELS; pix += (6 * NUM_ACTIVE_GROUPS)) {
+					if (state >> 3 == 0) {
+						Serial.println(pix);
+						// direct control
+						setDirectControlPixel(group, pix, (state & 0b100) != 0);
+						setDirectControlPixel(group, pix + 2, (state & 0b010) != 0);
+						setDirectControlPixel(group, pix + 4, (state & 0b001) != 0);
+					} else if (state == 0b1000) {
+						// cone
+						setGroup(pix, YELLOW_HUE);
+					} else if (state == 0b1001) {
+						// cube
+						setGroup(pix, PURPLE_HUE);
+					} else {
+						Serial.println("sus");
+					}
+				}
+			}
+			strip.show();
 			break;
-    case 6:  //cone 
-    	fillStrip_RGB(236,125,12);
-    	break;
-    // off
-    case 7:
-    	rave(255,255);
-    	break;
-    case 8:
-    	breathe(RED_HUE, SATURATION ,VALUE);
-    	break;
-    case 1: //cone
-    default:
-      strip.clear();
-      strip.show();
-      break;
-  }
+
+		// disabled
+		case 3:
+			breathingComet(RED_HUE, SATURATION, VALUE);
+			break;
+		case 4:
+			breathingComet(BLUE_HUE, SATURATION, VALUE);
+			break;
+
+		// off
+		case 1:
+		default:
+			strip.clear();
+			strip.show();
+			break;
+	}
 }
